ac340d7b9a0421a2dbf4c923c9922f24f66d0126
danielFeliseed <daniel@feliseed.co.jp>
2024-11-09 23:22:35 +0900

    fixed contact us scroll issue from other pages

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   const contactFormRef = useRef(null);
   const [formData, setFormData] = useState({ name: "", email: "", subject: "", message: "" });
   const [status, setStatus] = useState({ message: "", type: "" });
-  const [toastIsVisible, setToastIsVisible] = useState(false); 
+  const [toastIsVisible, setToastIsVisible] = useState(false);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -218,26 +218,49 @@ export default function Home() {
           Experience the freshness firsthand! We participate in farmers markets around the area every week.
         </p>
         <div className="grid gap-8 sm:gap-12 md:gap-16 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-          <div className="bg-white shadow-lg rounded-lg p-6 text-center transition-transform duration-300 hover:scale-105">
+
+          <div className="bg-white shadow-lg rounded-lg p-6 text-center transition-transform duration-300 hover:scale-105 flex flex-col justify-between">
             <Image src="/IMG_9713.JPG" alt="City Market" width={300} height={200} className="rounded-lg object-cover w-full h-[200px] mb-4" />
             <h3 className="text-2xl font-bold text-gray-800">Midtown Farmers Market</h3>
             <p className="text-gray-600 mt-2">Downtown Main Square</p>
-            <p className="text-gray-700 mt-2 font-medium">Saturdays: 9:00 AM - 2:00 PM</p>
-            <Link href="#" className="text-green-600 mt-4 inline-block font-semibold">See Details</Link>
+            <p className="text-gray-700 mt-2 font-medium flex-grow">Saturdays: 9:00 AM - 2:00 PM</p>
+            <a
+              href="https://www.google.com/maps/search/?api=1&query=Downtown+Main+Square"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-green-600 mt-4 inline-block font-semibold"
+            >
+              Get Directions
+            </a>
           </div>
-          <div className="bg-white shadow-lg rounded-lg p-6 text-center transition-transform duration-300 hover:scale-105">
+
+          <div className="bg-white shadow-lg rounded-lg p-6 text-center transition-transform duration-300 hover:scale-105 flex flex-col justify-between">
             <Image src="/argicenter2.jpeg" alt="Parkside Market" width={300} height={200} className="rounded-lg object-cover w-full h-[200px] mb-4" />
             <h3 className="text-2xl font-bold text-gray-800">Cordova Argicenter Farmers Market</h3>
             <p className="text-gray-600 mt-2">Green Park, West Pavilion</p>
             <p className="text-gray-700 mt-2 font-medium">Sundays: 8:00 AM - 1:00 PM</p>
-            <Link href="#" className="text-green-600 mt-4 inline-block font-semibold">See Details</Link>
+            <a
+              href="https://www.google.com/maps/search/?api=1&query=Green+Park,+West+Pavilion"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-green-600 mt-4 inline-block font-semibold"
+            >
+              Get Directions
+            </a>
           </div>
-          <div className="bg-white shadow-lg rounded-lg p-6 text-center transition-transform duration-300 hover:scale-105">
+          <div className="bg-white shadow-lg rounded-lg p-6 text-center transition-transform duration-300 hover:scale-105 flex flex-col justify-between">
             <Image src="/farm4.jpg" alt="Riverfront Market" width={300} height={200} className="rounded-lg object-cover w-full h-[200px] mb-4" />
             <h3 className="text-2xl font-bold text-gray-800">Lightfoot Farmers Market</h3>
             <p className="text-gray-600 mt-2">Riverfront Plaza</p>
             <p className="text-gray-700 mt-2 font-medium">Wednesdays: 3:00 PM - 7:00 PM</p>
-            <Link href="#" className="text-green-600 mt-4 inline-block font-semibold">See Details</Link>
+            <a
+              href="https://www.google.com/maps/search/?api=1&query=Riverfront+Plaza"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-green-600 mt-4 inline-block font-semibold"
+            >
+              Get Directions
+            </a>
           </div>
         </div>
       </section>
--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,65 +1,38 @@
 import Link from 'next/link';
 
 export default function Navbar() {
-    return (
-      <div className="navbar bg-white shadow-md border-b px-3 py-4">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 text-black"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] w-52 bg-white text-black rounded-box shadow"
+  return (
+    <div className="navbar bg-white shadow-md border-b px-3 py-4">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 text-black"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              <li>
-                <Link href="/products">Products</Link>
-              </li>
-              <li>
-                <details>
-                  <summary>Socials</summary>
-                  <ul className="p-2 bg-pearl-white">
-                    <li>
-                      <a target='_blank' href="https://www.facebook.com/fensterfarms">Facebook</a>
-                    </li>
-                    <li>
-                      <a target='_blank' href="https://www.youtube.com/@FensterfarmGreenhouse">YouTube</a>
-                    </li>
-                  </ul>
-                </details>
-              </li>
-            </ul>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
           </div>
-          <Link href="/" className="btn btn-ghost normal-case text-2xl font-bold text-black">
-            Fenster Farm
-          </Link>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1 text-black">
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] w-52 bg-white text-black rounded-box shadow"
+          >
             <li>
-                <Link href="/products">Products</Link>
+              <Link href="/products">Products</Link>
             </li>
-            {/* <li>
-              <a>Our Story</a>
-            </li> */}
             <li>
               <details>
                 <summary>Socials</summary>
-                <ul className="p-2 bg-pearl-white z-40 text-black">
-                  <li className="">
+                <ul className="p-2 bg-pearl-white">
+                  <li>
                     <a target='_blank' href="https://www.facebook.com/fensterfarms">Facebook</a>
                   </li>
                   <li>
@@ -70,12 +43,38 @@ export default function Navbar() {
             </li>
           </ul>
         </div>
-        <div className="navbar-end">
-          <a href="#contact_form" className="btn-info hidden sm:block">
-            Contact Us
-          </a>
-        </div>
+        <Link href="/" className="btn btn-ghost normal-case text-2xl font-bold text-black">
+          Fenster Farm
+        </Link>
       </div>
-    );
-  }
-  
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 text-black">
+          <li>
+            <Link href="/products">Products</Link>
+          </li>
+          {/* <li>
+              <a>Our Story</a>
+            </li> */}
+          <li>
+            <details>
+              <summary>Socials</summary>
+              <ul className="p-2 bg-pearl-white z-40 text-black">
+                <li className="">
+                  <a target='_blank' href="https://www.facebook.com/fensterfarms">Facebook</a>
+                </li>
+                <li>
+                  <a target='_blank' href="https://www.youtube.com/@FensterfarmGreenhouse">YouTube</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end ">
+        <Link href="/#contact_form" scroll={true}>
+          <button className="btn hidden sm:block">Contact Us</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
